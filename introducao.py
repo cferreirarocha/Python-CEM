21019cc577f832111a2038f73a469b0643625c54
cferreirarocha <48653477+cferreirarocha@users.noreply.github.com>
2020-04-13 19:44:53 -0300

    Add files via upload

---
+++ introducao.py
@@ -0,0 +1,53 @@
+##print('Olá, mundo'); #quebrando a maldição
+
+
+#print(7+4) #soma sem aspas
+
+#print('7'+'4') #com aspas ele junta os dois numero, porém não reconhece como numero
+
+#print('Olá', 5)
+
+#nome = 'Guanabara'
+#idade = 25
+#peso = 75.8
+
+#print(nome, idade, peso)
+
+#nome = input('Qual é seu nome? ')
+#idade = input('Quantos anos você tem? ')
+#peso = input('Quanto você pesa? ')
+
+#print(nome, idade, peso)
+
+
+#Desafios 
+
+#1 - Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+#2 - Crie um script Python que leia o dia e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada. 
+#3 - Crie um script Python que leia dois numeros e tente mostrar a soma entre eles.
+
+#1
+#nome = input('Qual é seu nome? ')
+
+#print(nome + ', Seja bem-vindo!')
+
+
+#2
+
+#dia = input('Que dia você nasceu? ')
+#mes = input('Que mes você nasceu? ')
+#ano = input('Que ano você nasceu? ')
+
+#print('Você nasceu no ', dia, mes, ano, 'correto?')
+
+
+#3
+
+num1  = int (input('Digite um numero? '))
+num2  = int (input('Digite outro numero? '))
+soma = num1 + num2
+
+print('A soma dos valores é' , soma)
+
+
+
