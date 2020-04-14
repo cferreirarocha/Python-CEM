83e954b05b43571ab8e66d93592c62d8c2148e2a
cferreirarocha <48653477+cferreirarocha@users.noreply.github.com>
2020-04-13 22:11:15 -0300

    Add files via upload

---
+++ aula6testes.py
@@ -0,0 +1,61 @@
+"""
+EXPLICANDO SOBRE VARIAVEIS
+
+n1 = input('Digite um valor') Mostrando o tipo da variavel como não definiu o tipo primitivo retornou str
+print(type(n1))
+"""
+
+"""
+n1 = int(input('Digite um valor ')) #defini
+ndo a variavel como int
+print(type(n1))
+ 
+"""
+"""
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+
+print('A soma vale', s)
+
+"""
+
+"""
+n1 = int(input('Digite um valor: '))
+n2 = int (input('Digite outro valor: '))
+s = n1 + n2
+
+print(f'a soma entra {n1} e {n2} vale {s}')
+"""
+"""
+n = str(input('Digite um valor:'))
+print(type(n))
+"""
+"""
+n = str(input('Digite um valor:')) #vericiando o tipo primitivo 
+print(type(n))
+
+"""
+
+"""
+n = input('digite algo:' ) #boolean = True ou False
+print(n.isnumeric())
+"""
+
+"""
+n = input('Digite algo: ') #verificando se é alfabetico 
+print(n.isalpha())
+ 
+ """
+"""
+n = input('Digite algo: ') #verificar se alfanumerico 
+print(n.isalnum())
+
+"""
+
+"""
+n = input('Digite algo: ') # verifica se está em letra maiscula
+print(n.isupper())
+
+"""
+
