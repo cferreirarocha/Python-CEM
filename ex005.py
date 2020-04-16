c445053a4cab40a807d6915956982578fa79ce08
cferreirarocha <48653477+cferreirarocha@users.noreply.github.com>
2020-04-15 22:30:12 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,13 @@
+#005 - Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+"""
+n1 = int(input('Digite um numero: ')) #com mais de 1 variavel 
+
+s = n1 + 1
+a = n1 - 1
+
+print(f'O número digitado é {n1} e seu antecessor é {a} e seu sucessor é {s}! ')
+"""
+
+n = int(input('Digite um número: ')) #com apenas uma variavel
+
+print(f'O numero digitado é {n} e seu antecessor é {n - 1} e seu sucessor é {n + 1}')
