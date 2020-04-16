f82a1519d6583877b475f36552df4d5bc6e95577
cferreirarocha <48653477+cferreirarocha@users.noreply.github.com>
2020-04-15 23:52:14 -0300

    Add files via upload

--- ex007.py
+++ ex007.py
@@ -4,4 +4,4 @@ n1 = float(input('Digite a primeira nota do aluno: '))
 n2 = float(input('Digite a segunda nota do aluno: '))
 m = (n1 + n2) / 2
 
-print(f'A média entre {n1} e {n2} é igual {m}')
+print(f'A média entre {n1:.1f} e {n2:.1f} é igual {m:.1f}')
