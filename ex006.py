eb65313fea60da7885592923d85ac6ee2d6c5622
cferreirarocha <48653477+cferreirarocha@users.noreply.github.com>
2020-04-15 22:50:19 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,14 @@
+#006 - Crie um algortimo que leia um número e mostre o seu dobro triplo e raiz quadrada
+"""
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
+print(f'Número digitado {n}, dobro do número digitado {d}, triplo {t} e a raiz quadrada {r}')
+"""
+# Com uma varíavel 
+
+n = int(input('Digite um número: '))
+
+print(f'Número digitado é {n}, o dobro do número digitado {n * 2}, triplo {n * 3} e a raiz quadrada é {n ** (1/2)}')
